055f6114841c71f5e9465a880d41d8a2bd412548
andrehmassis <andre.hmassis@gmail.com>
2023-06-18 20:09:40 -0600

    new URI

--- server/config/db.js
+++ server/config/db.js
@@ -8,6 +8,7 @@ Date: 2023-06-17
 module.exports =
 {
  //"URI": "mongodb://localhost/contact_list"
- "URI": "mongodb+srv://andreassis:Lkp4GD9OfWyScBow@cluster0.feugqzu.mongodb.net/?retryWrites=true&w=majority"
+ //"URI": "mongodb+srv://andreassis:Lkp4GD9OfWyScBow@cluster0.feugqzu.mongodb.net/?retryWrites=true&w=majority"
+ "URI": "mongodb+srv://andreassis:NELWle2zNzkzVag9@mongodbserver.ymtnka7.mongodb.net/?retryWrites=true&w=majority"
 
 }
