680efed4765371bbd28b4e256f73847b246c74d2
andrehmassis <andre.hmassis@gmail.com>
2023-06-18 18:39:30 -0600

    update assignment2

--- server/config/db.js
+++ server/config/db.js
@@ -7,5 +7,6 @@ Date: 2023-06-17
 
 module.exports =
 {
-    "URI": "mongodb://localhost/contact_list"
+ "URI": "mongodb://localhost/contact_list"
+ //"URI" : "mongodb+srv://andreassis:Eh118sdohXkiALW0@mongodbserver.odoxu7y.mongodb.net/?retryWrites=true&w=majority"
 }
